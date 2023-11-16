e11ba764181162b669f1253a4f89462f0c4dbade
JustNorman <norman2and2@gmail.com>
2023-11-16 11:29:08 -0600

    Started adding the INSERT fucntions

---
+++ INSERT.sql
@@ -0,0 +1,13 @@
+-- Insert data into CHEF_CERT table
+INSERT INTO [CHEF_CERT] ([CERT_ID], [CERT_FEE], [CERT_DATE_OFFER])
+VALUES
+  (1, 100.00, '2023-01-15'), --Placeholders for the values to be inserted
+  (2, 150.00, '2023-02-20'),
+  (3, 120.00, '2023-03-10');
+
+-- Insert data into CHEF table
+INSERT INTO [CHEF] ([EMP_ID])
+VALUES
+  (101), --Placeholders for the values to be inserted
+  (102),
+  (103);
